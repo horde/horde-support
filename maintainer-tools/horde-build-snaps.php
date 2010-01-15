e298252498c11379b555725ff1859bf3697004af
Michael J. Rubinsky <mrubinsk@horde.org>
2010-01-15 15:04:04 -0500

    fix parse error

--- maintainer-tools/horde-build-snaps.php
+++ maintainer-tools/horde-build-snaps.php
@@ -28,7 +28,7 @@ $apps = array(
         'framework',
         'gollem',
         'horde',
-        'imp'
+        'imp',
         'ingo',
         'jeta',
         'kronolith',
@@ -96,7 +96,7 @@ system("ln -sfh $dir latest");
 function tarballGit($dir, $module, $repo, $name)
 {
     global $git_dir;
-    
+
     $filename = $module . '-' . $name . '.tar.gz';
     system('cd ' . $dir . '; ' . $git_dir . ' --git-dir=' . escapeshellarg($repo) . ' archive --format=tar --prefix=' . $module . '/ HEAD:' . $module . '/ | gzip -9 > ' . $filename);
     system('cd ' . $dir . '; ' . $GLOBALS['md5_path'] . ' ' . $filename . ' > ' . $filename . '.md5sum');
