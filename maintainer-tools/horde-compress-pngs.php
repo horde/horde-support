37853b4a435f76d96bef0abd6d046bc5c32db577
Michael M Slusarz <slusarz@curecanti.org>
2011-01-04 15:12:34 -0700

    Compress PNG script improvements
    Add option to specify directory
    Don't print compressed filenames to local file.

--- maintainer-tools/horde-compress-pngs.php
+++ maintainer-tools/horde-compress-pngs.php
@@ -4,7 +4,9 @@
  * Script to recursively find all PNG's in a directory and attempt to max
  * compress them.
  *
- * Usage: horde-compress-pngs.php -a [advpng binary] -o [optipng binary]
+ * Usage: horde-compress-pngs.php -a [advpng binary]
+ *                                -d [directory]
+ *                                -o [optipng binary]
  *
  * @category Horde
  * @package  maintainer_tools
@@ -15,16 +17,22 @@ require 'Console/Getopt.php';
 $c = new Console_Getopt();
 $argv = $c->readPHPArgv();
 array_shift($argv);
-$options = $c->getopt2($argv, 'a:o:');
+$options = $c->getopt2($argv, 'a:d:o:');
 if (PEAR::isError($options)) {
     print "Invalid arguments.\n";
     exit;
 }
 
+$directory = getcwd();
+
 foreach ($options[0] as $val) {
     switch ($val[0]) {
     case 'a':
         $advpng_binary = $val[1];
+        break;
+
+    case 'd':
+        $directory = $val[1];
         break;
 
     case 'o':
@@ -37,17 +45,12 @@ if (empty($advpng_binary) || empty($optipng_binary)) {
     exit("Invalid arguments.\n");
 }
 
-$processed = array();
-
-$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(getcwd(), RecursiveIteratorIterator::SELF_FIRST));
+$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory, RecursiveIteratorIterator::SELF_FIRST));
 while ($it->valid()) {
     if ($it->isFile() &&
         (strcasecmp('.png', substr($it->key(), -4)) === 0)) {
         system($advpng_binary . ' -z4 ' . $it->key());
         system($optipng_binary . ' -o7 ' . $it->key());
-        $processed[] = $it->key();
     }
     $it->next();
 }
-
-file_put_contents('png-compress.txt', implode("\n", $processed));
