818a6cad28c821a81cd3d842bf378696fed8675d
Jan Schneider <jan@horde.org>
2011-05-19 16:29:03 +0000

    Make it work with spaces in the path. Fix for webmail.

--- maintainer-tools/horde-build-website-docs.sh
+++ maintainer-tools/horde-build-website-docs.sh
@@ -25,20 +25,21 @@ echo > $FILEROOT/errors.txt
 
 # Search for docs directories
 echo "Searching applications..."
-DOC_DIRS=$(find $SOURCEDIR/ -name docs -a -type d)
+IFS=$'\012'
+DOC_DIRS=$(find "$SOURCEDIR/" -name docs -a -type d)
 echo "Generating documentation..."
 for DOC_DIR in $DOC_DIRS; do
     APP=$(basename $(dirname $DOC_DIR))
     if [ $APP != ".." ] &&
-        [ -f $DOC_DIR/../README ] &&
+        [ -f "$DOC_DIR/README" -o -f "$DOC_DIR/../README" ] &&
         [ "$1" = "" -o "$1" = "$APP" ]; then
         echo -n "$APP "
         APPROOT=$FILEROOT/app/views/App/apps/$APP/docs
         mkdir -p $APPROOT
         # Generate HTML docs
         DOCS=$APPROOT/docs.html
-        FILES=$(find $DOC_DIR -maxdepth 1 -type f -regex .*/[A-Z_]+ | sort)
-        FILES="$DOC_DIR/../README $FILES"
+        FILES=$(find "$DOC_DIR" -maxdepth 1 -type f -regex .*/[A-Z_]+ | sort)
+        FILES="$DOC_DIR/../README"$'\n'"$FILES"
         cat > $DOCS <<EOF
 <h3>Documentation</h3>
 
@@ -48,16 +49,16 @@ for DOC_DIR in $DOC_DIRS; do
 EOF
 
         for FILE in $FILES; do
-            if [ $(basename $FILE) = "CHANGES" ]; then
+            if [ $(basename "$FILE") = "CHANGES" ]; then
                 CHANGES=$APPROOT/CHANGES.html
                 echo '<h3>Changes by Release</h3><pre>' > $CHANGES
-                cat $FILE | sed 's/</\&lt;/g' | sed 's;pear\s*\(bug\|request\)\s*#\([[:digit:]]*\);<a href="http://pear.php.net/bugs/bug.php?id=\2">\0</a>;gi' | sed 's;\(,\s*\|(\)\(\(bug\|request\)\s*#\([[:digit:]]*\)\);\1<a href="http://bugs.horde.org/ticket/\4">\2</a>;gi' >> $CHANGES
+                cat "$FILE" | sed 's/</\&lt;/g' | sed 's;pear\s*\(bug\|request\)\s*#\([[:digit:]]*\);<a href="http://pear.php.net/bugs/bug.php?id=\2">\0</a>;gi' | sed 's;\(,\s*\|(\)\(\(bug\|request\)\s*#\([[:digit:]]*\)\);\1<a href="http://bugs.horde.org/ticket/\4">\2</a>;gi' >> $CHANGES
                 echo '</pre>' >> $CHANGES
                 echo -n .
                 echo "<li><a href=\"$WEBROOT/apps/$APP/docs/CHANGES\">CHANGES</a></li>" >> $DOCS
                 continue
             fi
-            if [ $(basename $FILE) = "RELEASE_NOTES" ]; then
+            if [ $(basename "$FILE") = "RELEASE_NOTES" ]; then
                 NOTES=$APPROOT/RELEASE_NOTES.html
                 echo '<h3>Release notes for the latest release</h3><pre>' > $NOTES
                 php -r "class n { function n() { require_once 'Horde/Release.php'; include '$FILE'; echo \$this->notes['ml']['changes']; } } new n;" | sed 's/</\&lt;/g' >> $NOTES
@@ -67,19 +68,19 @@ EOF
                 continue
             fi
             echo -n .
-            TO=$(basename $FILE .txt).html
+            TO=$(basename "$FILE" .txt).html
             OUTPUT=$($GENERATOR \
                 --output-encoding=UTF-8 \
                 --rfc-references \
-                $FILE \
+                "$FILE" \
                 2>>$FILEROOT/errors.txt)
             if [ $? ]; then
                 OUTPUT=$(echo "$OUTPUT" | sed '1,/<body>/d' | sed '/<\/body>/,$d');
                 echo "$OUTPUT" > $APPROOT/$TO;
                 if [ "$(uname)" = "FreeBSD" ]; then
-                    DISPLAY=$(echo $FILE | sed -r "s|$DOC_DIR/(\.\./)?||")
+                    DISPLAY=$(echo "$FILE" | sed -r "s|$DOC_DIR/(\.\./)?||")
                 else
-                    DISPLAY=$(echo $FILE | sed "s|$DOC_DIR/\(../\)\?||")
+                    DISPLAY=$(echo "$FILE" | sed "s|$DOC_DIR/\(../\)\?||")
                 fi
                 echo "<li><a href=\"$WEBROOT/apps/$APP/docs/$DISPLAY\">$DISPLAY</a></li>" >> $DOCS
             fi
