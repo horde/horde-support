91db3a93983526107bdba9ff3310c6efc0295868
Michael M Slusarz <slusarz@curecanti.org>
2011-11-25 01:37:52 -0700

    Improve PNG compression
    Add pngout step.
    Do compression by each tool twice, to pick up on optimizations that may
    have been unearthed by a subsequent command.
    Make results display much more succinct.

--- maintainer-tools/horde-compress-pngs.php
+++ maintainer-tools/horde-compress-pngs.php
@@ -7,8 +7,16 @@
  * Usage: horde-compress-pngs.php -a [advpng binary]
  *                                -d [directory]
  *                                -o [optipng binary]
+ *                                -p [pngout binary]
  *
+ * Copyright 2011 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @author   Michael Slusarz <slusarz@horde.org>
  * @category Horde
+ * @license  http://www.fsf.org/copyleft/lgpl.html LGPL
  * @package  maintainer_tools
  */
 
@@ -17,7 +25,7 @@ require 'Console/Getopt.php';
 $c = new Console_Getopt();
 $argv = $c->readPHPArgv();
 array_shift($argv);
-$options = $c->getopt2($argv, 'a:d:o:');
+$options = $c->getopt2($argv, 'a:d:o:p:');
 if (PEAR::isError($options)) {
     print "Invalid arguments.\n";
     exit;
@@ -38,10 +46,14 @@ foreach ($options[0] as $val) {
     case 'o':
         $optipng_binary = $val[1];
         break;
+
+    case 'p':
+        $pngout_binary = $val[1];
+        break;
     }
 }
 
-if (empty($advpng_binary) || empty($optipng_binary)) {
+if (empty($advpng_binary) || empty($optipng_binary) || empty($pngout_binary)) {
     exit("Invalid arguments.\n");
 }
 
@@ -49,8 +61,23 @@ $it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory, R
 while ($it->valid()) {
     if ($it->isFile() &&
         (strcasecmp('.png', substr($it->key(), -4)) === 0)) {
-        system($advpng_binary . ' -z4 ' . $it->key());
-        system($optipng_binary . ' -o7 ' . $it->key());
+        $start_size = $it->getSize();
+
+        system($advpng_binary . ' -z4 -q ' . $it->key());
+        system($optipng_binary . ' -zc1-9 -zm1-9 -zs0-3 -f0-5 -quiet ' . $it->key());
+        system($pngout_binary . ' ' . $it->key() . ' -y -q');
+
+        system($optipng_binary . ' -zc1-9 -zm1-9 -zs0-3 -f0-5 -quiet ' . $it->key());
+        system($advpng_binary . ' -z4 -q ' . $it->key());
+        system($pngout_binary . ' ' . $it->key() . ' -y -q');
+
+        clearstatcache();
+        $end_size = $it->getSize();
+
+        if ($start_size > $end_size) {
+            print $it->key() . ":\n" .
+                  '    ' . $start_size . ' => ' . $end_size . ' (' . round((($start_size - $end_size) / $start_size) * 100, 1) . "%)\n";
+        }
     }
     $it->next();
 }
