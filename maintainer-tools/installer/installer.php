01002aa247264dfea21ce15a963365e5607f940c
Michael M Slusarz <slusarz@curecanti.org>
2013-10-18 22:07:45 -0600

    phpdoc header

--- maintainer-tools/installer/installer.php
+++ maintainer-tools/installer/installer.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Horde installer script.
+ *
+ * Copyright 2013 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ *
+ * @author    Michael Slusarz <slusarz@horde.org>
+ * @category  Horde
+ * @copyright 2013 Horde LLC
+ * @license   http://www.fsf.org/copyleft/gpl.html GPL
+ */
 
 require 'vendor/autoload.php';
 
