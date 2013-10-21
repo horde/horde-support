3c929ac58e549e060bde14be31e70179e4677569
Michael M Slusarz <slusarz@curecanti.org>
2013-10-21 01:26:12 -0600

    Silence timezone warnings

--- maintainer-tools/installer/lib/HordeInstaller.php
+++ maintainer-tools/installer/lib/HordeInstaller.php
@@ -6,6 +6,9 @@ class HordeInstaller
 
     public function initLogger($logfile)
     {
+        // Silence timezone errors.
+        date_default_timezone_set(@date_default_timezone_get());
+
         $this->log = new Horde_Log_Logger(new Horde_Log_Handler_Null());
         $this->log->addHandler(new Horde_Log_Handler_Stream($logfile));
     }
