7d4b0f01824e8716602160252247b5fd3643b19d
Ralf Lang <ralf.lang@ralf-lang.de>
2025-05-09 09:44:10 +0200

    Convert to curl command as we don't want wget in the image

--- dev-php84-ldap/images/php8.4-apache-horde/01-install-tools.sh
+++ dev-php84-ldap/images/php8.4-apache-horde/01-install-tools.sh
@@ -6,13 +6,13 @@ php -r "unlink('composer-setup.php');"
 
 gh release download --clobber -R horde/components -p 'horde-components.phar' -D ./tools/
 gh release download --clobber -R phpstan/phpstan -p 'phpstan.phar' -D ./tools/
-wget -O /usr/local/tools/phive.phar "https://phar.io/releases/phive.phar" 
-wget -O /usr/local/tools/phpunit-12.phar https://phar.phpunit.de/phpunit-12.phar &> /dev/null
+curl -o /usr/local/tools/phive.phar "https://phar.io/releases/phive.phar" 
+curl -o /usr/local/tools/phpunit-12.phar https://phar.phpunit.de/phpunit-12.phar &> /dev/null
 echo "✅ Downloaded composer, horde-components, phpstan and phive"
-wget -O /usr/local/tools/phpunit-11.phar https://phar.phpunit.de/phpunit-11.phar &> /dev/null
-wget -O /usr/local/tools/phpunit-10.phar https://phar.phpunit.de/phpunit-10.phar &> /dev/null
-wget -O /usr/local/tools/phpunit-9.phar https://phar.phpunit.de/phpunit-9.phar   &> /dev/null
+curl -o /usr/local/tools/phpunit-11.phar https://phar.phpunit.de/phpunit-11.phar &> /dev/null
+curl -o /usr/local/tools/phpunit-10.phar https://phar.phpunit.de/phpunit-10.phar &> /dev/null
+curl -o /usr/local/tools/phpunit-9.phar https://phar.phpunit.de/phpunit-9.phar   &> /dev/null
 echo "✅ Downloaded phpunit 12, 11, 10 and 9"
 chmod +x /usr/local/tools/*.phar
-cp -rs /usr/local/tools/* /usr/local/bin" &> /dev/null
+cp -rs /usr/local/tools/* /usr/local/bin &> /dev/null
 echo "✅ Linked all phar tools to /usr/local/bin"
